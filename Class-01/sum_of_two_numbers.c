ccc9e3ae490d08324c70d8bd352283032e4e1ce2
Abdul Kayum <kayum12123@gmail.com>
2026-01-30 11:10:11 +0600

    Add sum of two numbers program (Class 01)

---
+++ Class-01/sum_of_two_numbers.c
@@ -0,0 +1,15 @@
+/*
+    Course      : Structure Programming Lab
+    Course Code : CSE 104
+    Class       : 01
+    Program     : Sum of two numbers
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a + b);
+    return 0;
+}
