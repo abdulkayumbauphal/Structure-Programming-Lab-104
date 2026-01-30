fa661c0842a0e8eafe05f49143d80e771c33db99
Abdul Kayum <kayum12123@gmail.com>
2026-01-30 11:07:55 +0600

    Add Hello World program (Class 01)

---
+++ Class-01/hello_world.c
@@ -0,0 +1,13 @@
+/*
+    Course      : Structure Programming Lab
+    Course Code : CSE 104
+    Class       : 01
+    Program     : Hello World
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    return 0;
+}
