d734b2e707d2bb0e9698c66e79621ea55f291c03
Abdul Kayum <kayum12123@gmail.com>
2026-01-31 11:48:43 +0600

    Add files via upload

---
+++ Class-01/char_to_ascii.c
@@ -0,0 +1,16 @@
+/*
+    Course      : Structure Programming Lab
+    Course Code : CSE 104
+    Class       : 01
+    Program     : char to acii
+*/
+
+#include <stdio.h>
+
+int main() {
+    char c;
+    printf("Enter any character : ");
+    scanf("%c",&c);
+    printf("Char %c ascii value is : %d",c,c);
+    return 0;
+}
