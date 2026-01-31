bbc606ee40335f582c8fd584205dfffb854d1afc
Abdul Kayum <kayum12123@gmail.com>
2026-01-31 11:53:37 +0600

    Create ascii_to_char.c

---
+++ Class-01/ascii_to_char.c
@@ -0,0 +1,16 @@
+/*
+    Course      : Structure Programming Lab
+    Course Code : CSE 104
+    Class       : 01
+    Program     : Ascii to Character
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter any Integer : ");
+    scanf("%d",&n);
+    printf("Integer %d Character value is : %c",n,n);
+    return 0;
+}
