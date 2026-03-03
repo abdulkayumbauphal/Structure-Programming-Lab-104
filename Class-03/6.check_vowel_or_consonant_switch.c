9cdec1500718da6e40774e6f41389ca7de1f3347
Abdul Kayum <kayum12123@gmail.com>
2026-03-04 00:38:45 +0600

    Add files via upload

---
+++ Class-03/6.check_vowel_or_consonant_switch.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    char ch;
+    printf("Enter an alphabet: ");
+    scanf("%c", &ch);
+    switch(ch) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            printf("It is a Vowel\n");
+            break;
+        default:
+            printf("It is a Consonant\n");
+    }
+    return 0;
+}
